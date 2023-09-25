cf973c66df5ee3d2bb6b56fea7b80accde903cc1
Putra1688 <ranggadsaputra11@gmail.com>
2023-09-25 14:52:22 +0700

    new file

---
+++ Lat1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Lat1{
+    public static void main(String[] args){
+        Scanner sc23 = new Scanner(System.in);
+
+        System.out.println("Masukkan suhu : ");
+        int suhu = sc23.nextInt();
+
+        if (suhu<16) {
+            System.out.println("Silahkan menggunakan jaket");
+        }
+        if (suhu<20) {
+            System.out.println("Silahkan pakai baju tebal");
+        }
+        else {
+            System.out.println("Silahkan pakai topi");
+        }
+    }
+}
