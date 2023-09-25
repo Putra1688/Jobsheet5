99b8fa06b70f446ce87374ac5deda01b26543cc3
Putra1688 <ranggadsaputra11@gmail.com>
2023-09-25 16:02:41 +0700

    baru

--- PercobaanPemilihan223.java
+++ PercobaanPemilihan223.java
@@ -13,7 +13,14 @@ import java.util.Scanner;
             float tugas = input23.nextFloat();
 
             float total = (uas*0.4f) + (uts*0.3f) + (kuiz*0.1f) + (tugas*0.1f);
-            String message = total < 65 ? "Remidi" : "Tidak Remidi";
+            String message = 80 < total && total <= 100 ? "Sangat Baik"
+                : 73 < total && total <= 80 ? "Lebih Baik"
+                : 65 < total && total <= 73 ? "Baik"
+                : 60 < total && total <= 65 ? "Lebih dari Cukup"
+                : 50 < total && total <= 60 ? "Cukup"
+                : 39 < total && total <= 50 ? "Kurang"
+                : "Gagal";
+
             System.out.println("Nilai akhir = " +total + "sehingga" +message);
             }
     }
