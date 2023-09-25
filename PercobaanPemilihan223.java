07e9869ac80cf6fd3d4f2032c3ffbc6cc3f4a201
Putra1688 <ranggadsaputra11@gmail.com>
2023-09-25 16:16:00 +0700

    update baru

--- PercobaanPemilihan223.java
+++ PercobaanPemilihan223.java
@@ -13,14 +13,14 @@ import java.util.Scanner;
             float tugas = input23.nextFloat();
 
             float total = (uas*0.4f) + (uts*0.3f) + (kuiz*0.1f) + (tugas*0.1f);
-            String message = 80 < total && total <= 100 ? "Sangat Baik"
-                : 73 < total && total <= 80 ? "Lebih Baik"
-                : 65 < total && total <= 73 ? "Baik"
-                : 60 < total && total <= 65 ? "Lebih dari Cukup"
-                : 50 < total && total <= 60 ? "Cukup"
-                : 39 < total && total <= 50 ? "Kurang"
+            String message = 80 < total && total <= 100 ? "4:Sangat Baik(A)"
+                : 73 < total && total <= 80 ? "3.5:Lebih Baik(B+)"
+                : 65 < total && total <= 73 ? "3:Baik(B)"
+                : 60 < total && total <= 65 ? "2.5:Lebih dari Cukup(C+)"
+                : 50 < total && total <= 60 ? "2:Cukup(C)"
+                : 39 < total && total <= 50 ? "1.5:Kurang(D)"
                 : "Gagal";
 
-            System.out.println("Nilai akhir = " +total + "sehingga" +message);
+            System.out.println("Nilai akhir = " +total + " sehingga " +message);
             }
     }
